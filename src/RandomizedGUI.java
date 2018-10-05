91c1db28ce966889521b43d877e9376a3fe68a5c
Andrew Oppenheimer <aoppenh@purdue.edu>
2018-10-05 02:26:25 -0400

    new commit

--- src/RandomizedGUI.java
+++ src/RandomizedGUI.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 /**
  * Created by Andrew Oppenheimer on 12/25/2016.
- * Version 10/04/2018
+ * Version 10/05/2018
  */
 public class RandomizedGUI extends JFrame {
     private Container cPane;
